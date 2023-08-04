f9aa815b3543989d26a4d602a742c982f76ad3b1
manuuu123 <manojrajana554@gmail.com>
2023-08-04 02:10:10 -0700

    first commit

---
+++ Nursery-Backend/src/main/java/com/diet/api/NurseryRepo.java
@@ -0,0 +1,7 @@
+package com.diet.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NurseryRepo extends JpaRepository<Nursery, Integer> {
+
+}
---
+++ Nursery-Backend/src/main/java/com/diet/api/NurserySpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.diet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurserySpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurserySpringAppApplication.class, args);
+	}
+
+}
